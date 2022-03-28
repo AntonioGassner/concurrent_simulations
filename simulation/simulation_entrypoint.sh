31d7974d33ae9526d7103592bc27a48a365ca9b9
AntonioGassner <56674343+AntonioGassner@users.noreply.github.com>
2022-03-28 14:00:57 +0200

    agriculture installation fixed

--- simulation/simulation_entrypoint.sh
+++ simulation/simulation_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/robomaker/project_ccc
+cd /home/robomaker/dev_ws
 source /opt/ros/foxy/setup.bash
 source /usr/share/gazebo-11/setup.sh
 source ./install/setup.sh
@@ -7,9 +7,6 @@ export SVGA_VGPU10=0
 source /usr/share/colcon_cd/function/colcon_cd.sh
 export _colcon_cd_root=~/ros2_install
 export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11
-ros2 launch ccc_pkg simulation.launch.py &
-cd /home/robomaker/my_logs &
-ros2 bag record -o subset /dolly_1/cmd_vel &
-ros2 launch ccc_pkg controllers.launch.py 
+ros2 launch agriculture multi_launch.py &
 
 exec "${@:1}"
